b66858cf61c3f726071e57d6ca554340f5018a70
jseaman <jseaman@pointpark.edu>
2019-11-13 22:46:36 -0500

    Added files for app

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,14 @@
 const functions = require('firebase-functions');
+const express = require('express');
 
+const app = express();
+app.get('/timestamp', (request, response) => {
+  response.send('${Date.now()}');
+})
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.app = functions.https.onRequest(app);
+
+
+  //response.send("Hello from Firebase!");
