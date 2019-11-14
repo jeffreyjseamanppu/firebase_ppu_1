e1094c9405d1cbc1eafd22d8abdbb8514ea4d8c9
jseaman <jseaman@pointpark.edu>
2019-11-13 23:31:40 -0500

    Made changes

--- functions/index.js
+++ functions/index.js
@@ -3,8 +3,13 @@ const express = require('express');
 
 const app = express();
 app.get('/timestamp', (request, response) => {
-  response.send('${Date.now()}');
-})
+  response.send(`${Date.now()}`);
+});
+
+app.get('/timestamp-cached', (request, response) => {
+  response.set('Cache-Control','public, max-age=300, s-maxage=600');
+  response.sent(`${Date.now()}`);
+});
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
